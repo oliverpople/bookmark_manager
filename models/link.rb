a92a9d120b9ad6ddfdf7144f366d6909ef6f7a25
oliver pople <oliverpople@olivers-MacBook-Pro.local>
2017-10-19 12:37:04 +0100

    Removing proc file

--- models/link.rb
+++ models/link.rb
@@ -12,7 +12,5 @@ end
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
-
-
 DataMapper.finalize
 DataMapper.auto_upgrade!
